2e389e1bc2d474ca3bcd775f8ce234cbad395223
owuordickson <owuordickson@gmail.com>
2021-07-21 17:12:30 +0300

    Added

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+
+setup(
+    name='swarm-gp',
+    version='1.0',
+    description="A Python implementation of Swarm Optimization algorithms",
+    long_description=readme + '\n\n' + history,
+    author="Dickson Owuor",
+    author_email='owuordickson@ieee.org',
+    url='https://github.com/owuordickson/swarm_gp',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='swarm intelligence',
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Massachusetts Institute of Technology (MIT) License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
