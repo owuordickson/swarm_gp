fc7dce08e400a0f57a624f26ec0e786b581014a8
owuordickson <owuordickson@gmail.com>
2021-07-26 09:37:28 +0300

    Added

--- src/pkg_init/config.py
+++ src/pkg_init/config.py
@@ -4,7 +4,8 @@
 MIN_SUPPORT = 0.5
 CPU_CORES = 4
 # DATASET = "../../data/DATASET.csv"
-DATASET = "../../data/breast_cancer.csv"
-ALGORITHM = 'aco'
+# DATASET = "../../data/breast_cancer.csv"
+DATASET = "../../data/c2k_02k.csv"
+
 # Uncomment for Terminal:
 # DATASET = "data/DATASET.csv"
