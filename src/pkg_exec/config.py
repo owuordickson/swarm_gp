0e013dc4519aacbbea614b4ab37663927aefc481
owuordickson <owuordickson@gmail.com>
2021-08-10 09:17:11 +0300

    updated

--- src/pkg_exec/config.py
+++ src/pkg_exec/config.py
@@ -2,7 +2,7 @@
 
 # Configurations for Gradual Patterns:
 INITIALIZATIONS = 3
-ALGORITHM = 'prs'
+ALGORITHM = 'ga'
 MIN_SUPPORT = 0.5
 CPU_CORES = 4
 
@@ -10,10 +10,10 @@ CPU_CORES = 4
 # DATASET = "../../data/hcv_data.csv"
 
 # Uncomment for Main:
-# DATASET = "../data/hcv_data.csv"
+DATASET = "../data/hcv_data.csv"
 
 # Uncomment for Terminal:
-DATASET = "data/DATASET.csv"
+# DATASET = "data/DATASET.csv"
 
 # Global Swarm Configurations
 MAX_ITERATIONS = 100
